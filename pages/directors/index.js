de3c1c42cbccfe798531c205d7b1f1b91c19733e
Neeraj Kushwaha <neerajk.contact@gmail.com>
2022-11-25 15:10:19 +0530

    progress

--- pages/directors/index.js
+++ pages/directors/index.js
@@ -90,18 +90,21 @@ export default function Directors(){
                 <div className="OnBoard-wrapper">
                     <div className="OnBoard-listItems">
                         <OnBoardItem
+                            key={0}
                             imgSrc = "./assets/prosit_roy.png"
                             name = "Prosit Roy"
                             G_El_prop = {["#5541f8", "47s", "translate3d(calc(-10 * 0.69 * 1vw), calc(-10 * 0.69 * 1vw), 0px) rotate(1deg)"]}
                             about = {<>The king of nuances and backstories.<br/><br/>One of the main reasons Prosit&apos;s work is identifiable as his own is due to his ability to get to the root of every story, deep-diving into cultures and making the characters memorable and relatable.<br/><br/>He internalizes the script and beautifully decodes it on screen. Whether it is Patal Lok or a Pampers advertisement, Prosit has the expertise to evoke emotion from all audiences.<br/><br/>He&apos;s the most incredible support system for a team but an even better Director.</>}
                         />
                         <OnBoardItem
+                            key={1}
                             imgSrc = "./assets/jeet.png"
                             name = "Jeet Lotia"
                             G_El_prop = {["#af37d9", "43s", "translate3d(calc(10 * 0.69 * 1vw), calc(-10 * 0.69 * 1vw), 0px) rotate(1deg)"]}
                             about = {<>One that brings words to life.<br/>Our resident magician.<br/><br/>The sheer ease with which Jeet can tell a story off-screen translates directly to his work on screen with an innate ability to bring out the desired emotion impactfully.<br/><br/>An ad film director who&apos;s worked on over 150 commercials in the last seven years and earned all his tricks on-set, somewhere between reel life and real life<br/><br/>Jeet is a dire cinema addict obsessed with doing justice to every script.</>}
                         />
                         <OnBoardItem
+                            key={2}
                             imgSrc = "./assets/thea.png"
                             name = "Teodora Chingarova"
                             G_El_prop = {["#f2ad45", "44s", "translate3d(calc(-10 * 0.69 * 1vw), calc(10 * 0.69 * 1vw), 0px) rotate(100deg)"]}
